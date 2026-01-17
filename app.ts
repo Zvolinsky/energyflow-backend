8306b372f5670aa38afdf8428a0dbdc11e99af1f
NeonNygus <4derker@gmail.com>
2026-01-17 23:43:35 +0100

    chore: setup CORS

--- app.ts
+++ app.ts
@@ -1,10 +1,17 @@
 import express from 'express'
+import cors from 'cors'
 import {
     getEnergySummary,
     getOptimalWindow,
 } from '@/controllers/carbonController.ts'
 
 const app = express()
+
+let corsOptions = {
+    origin: ['http://localhost:5173'],
+}
+
+app.use(cors(corsOptions))
 
 app.get('/api/get-energy-mix', getEnergySummary)
 app.get('/api/get-optimal-window/:hours', getOptimalWindow)
